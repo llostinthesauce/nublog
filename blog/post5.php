c77935bba8e8a8fba9bb6fbf7dfeebfd097a12aa
llostinthesauce <cobyshanks@gmail.com>
2025-03-24 04:51:03 -0500

    n

--- blog/post5.php
+++ blog/post5.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nature being paradoxical - nuBlog</title>
     <link rel="stylesheet" href="/styles/style.css"> <!-- Link to external CSS -->
     <link rel="icon" href="/images/favicon.ico" type="image/x-icon"> <!-- Add favicon here -->
 </head>
