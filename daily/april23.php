86b56ed29fa1967e97c15457d56fae5a75105b12
llostinthesauce <cobyshanks@gmail.com>
2025-04-23 23:06:04 -0500

    m

--- daily/april23.php
+++ daily/april23.php
@@ -18,7 +18,17 @@
         <img src="/images/daily/0423.jpg" alt="x" class="styled-image2">
 
         <!-- Main text area -->
-        <p>loading</p>
+        <p>i made over $100 today. Yay! chipotle for dinner. they were out of chicken and chips and other stuff so it was a mystery carnitas bowl, but with a gc so that is good</p>
+        <p>i went to class just to see that no one was there. i guess class is done for the semester already. final projects ... </p>
+        <p>likely headed home this weekend. or maybe not, going off of vibes. hanging out with people is important but so is my family ... </p>
+        <p>been doing a deep clean and re-org of this studio before i install the new macs into their places. if i am lucky, i will be able to buy a old iMac or Mac pro from the university. please!!!!</p>
+        <p>found elliot smith and srde and some other albums on the shelf to burn to the iPod. only issue is that i dont have my superdrive. so i used the 27 inch 2011 iMacs disk drive to burn them in (same settings as i would on my MacBook) and they swapped them over on a flash drive. gonna import these now and Yay iPod all good except austin has my charger. Bro pleaseeee give it back ... </p>
+        <p>and today am really mulling over things and have been writing them down. really about perspective and how i think about specific things. not everything has to be approached in the same manner as it has before. i march thru time and my approach ought to change with that. even for things ive set high rules or expectations for. people are fluid in this sense and i am people</p>
+        <p>i talked to the "mormons" on campus today for like half an hour and am going to church soon. for the xp of course, but i respect their husltle. they were 18 and 19 and i learned that they genuinely skip going to college for 1-6 years to do missions across the country and world. crazy stuff, but sweet boys. they want to play pickleball too. they ofc were from Utah and i told them about my trip last year. and they got a kick out of me knowing and LOVING Costa Vida </p>
+        <p>carlos and i seriosuly sat in this office all day sorting, cleaning, throwing away, and derping. it is in a great place and it brings me great joy to bring Joy to places that other people will enjoy. a little passion goes a long way :) i love music and all of this old tech please god let me buy one at FMV </p>
+        <p>still losing $ on stocks. well options. the port is down 20% already since i opened it a few weeks ago. meanwhile my btc port is up 20% from the same time frame. i guess shorting trump is a bad idea but whatever. </p>
+        <p>i cant beleive panera is CONFIRMED for the student center. but i have a sneaky feeling that it isnt going to support sip club bc that is too OP. maybe not tho </p>
+        <p>last english class aside from stop week tomorrow. maybe i will cook if i go home. Been walking too, over 10k steps i think these last few days. good for the soul. </p>
 
         <?php include '../footer.php'; ?>
     </div>
