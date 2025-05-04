09d703e0952a87bc4753c1393f96842cc68a9d0c
llostinthesauce <cobyshanks@gmail.com>
2025-05-03 19:06:22 -0500

    edits

---
+++ daily/april27.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>27 April 2025 - nuBlog</title>
+    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+</head>
+<body class="blog-post-page"> 
+    <div class="content">
+        <?php include '../header.php'; ?> 
+
+        <h1 class="underlined-heading">27 April 2025</h1>
+        <div></div>
+
+        <!-- Optional image -->
+        <img src="/images/daily/0427.png" alt="x" class="styled-image2">
+
+        <!-- Main text area -->
+        <p>ripped tacos el gallo today. mmmm birria!!! 2001 space on 4k too! and a walk</p>
+
+        <?php include '../footer.php'; ?>
+    </div>
+</body>
+</html>
